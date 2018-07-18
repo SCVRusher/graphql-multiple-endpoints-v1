9a93ad735a605cb437a5fc19a62bf0ff78956c29
pro-i <pro-i@passber.com>
2018-07-18 11:23:26 +0800

    Update Apollo Engine
    - add format Error for debug
    - add response Error for debug
    - Set foundation for data tracking

--- graphql-multiple-endpoints-v1/index.js
+++ graphql-multiple-endpoints-v1/index.js
@@ -2,10 +2,24 @@ import { GraphQLServer } from 'graphql-yoga'
 import typeDefs from './schema/types'
 import resolvers from './resolvers/resolvers'
 
-//run graphql server here
+//apollo enginer api key 
+const APOLLO_ENGINE_KEY = "<ENTER YOUR KEY HERE>";
+
+//run graphql server with apollo stuff here
 const server = new GraphQLServer({
     typeDefs,
-    resolvers
+    resolvers,
+    engine: {
+        apiKey: APOLLO_ENGINE_KEY
+    },
+    formatError: error => {
+        console.log(error);
+        return error;
+    },
+    formatResponse: response => {
+        console.log(response);
+        return response;
+    },
 })
 
 server.start(() => console.log('Server is running.... '))
